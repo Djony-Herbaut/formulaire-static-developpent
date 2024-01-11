1d40244e6571e519ccfc929300371f111970e288
Djony-Herbaut <djonyandrejovinac@gmail.com>
2024-01-11 15:51:50 +0100

    mise en forme

--- index.php
+++ index.php
@@ -7,34 +7,11 @@
         <h1>Formulaire de contact</h1>
     </header>
     <main>
-        
-            <div class="inner-form" role="form">
-                
-           <!-- <ul class="response-server"></ul> espace reservé à la réponse bdd -->
-   
-            <fieldset>
-                <legend>Espace membre</legend>
-                <form action="index.php" method="post"> <!-- mettre le nom du fichier html dans aciton -->
-                    <label for="nom">Nom</label>
-                    <input type="text" id="nom" name="nom" placeholder="Nom" aria-required="true"  autofocus>
-
-                    <label for="prenom">Prénom</label>
-                    <input type="text" id="prenom" name="prenom" placeholder="Prénom" aria-required="true">
-
-                    <label for="date">Date de naissance</label>
-                    <input type="date" id="date" name="date" aria-required="true">
-        
-                    <label for="phone">Votre téléphone</label>
-
-                    <input type="tel" id="phone" name="phone" aria-label="format à 10 chiffre" placeholder="téléphone" aria-required="true">
-
-                    <small>Format: (+33)01-75-22-15-45</small>
-                    <button type="submit" value="Envoyer" aria-label="cliquez pour envoyer">envoyer</button>
-
-                </form>
-            </fieldset>
-        </div>
-        </main>
+        <?php
+            include_once __DIR__."/src/form-verif.inc.php"
+        ?>
+            
+    </main>
     <footer>
         <p>
             &copy; - PHP -  08/01/2024 
--- src/form-verif.inc.php
+++ src/form-verif.inc.php
@@ -1,8 +1,45 @@
-<?php
+<div class="inner-form" role="form">
+        <ul class="response-server">
+            <?php
+                if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['date']) && isset($_POST['phone'])){
 
-    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['date']) && isset($_POST['phone'])){
-        
+                    print "<pre>";
+                        print_r($_POST);
+                    print "</pre>";
                     
-
-    }
-?>
+               /*  foreach ($_POST as $key => $value) {
+                        if($_POST){
+                            print "<li>".$key." : ".$value."</li>\n";
+                        }
+                        else{
+                            die("<li>Erreur</li>\n");
+                    }
+                }  */  
+            
+                    if($_POST['nom'] && $_POST['prenom'] && $_POST['date'] && is_numeric($_POST['phone'])){
+                        echo "<li>".strip_tags($_POST['nom'])."</li>
+                        <li>".strip_tags($_POST['prenom'])."</li>
+                        <li>".$_POST['date']."</li>
+                        <li>".$_POST['phone']."</li>";
+                    }           
+                }
+            ?>
+        </ul>
+   
+        <fieldset>
+            <legend>Espace membre</legend>
+            <form action="index.php" method="post"> <!-- mettre le nom du fichier html dans aciton -->
+                <label for="nom">Nom</label>
+                <input type="text" id="nom" name="nom" placeholder="Nom" aria-required="true"  autofocus>
+                <label for="prenom">Prénom</label>
+                <input type="text" id="prenom" name="prenom" placeholder="Prénom" aria-required="true">
+                <label for="date">Date de naissance</label>
+                <input type="date" id="date" name="date" aria-required="true">
+    
+                <label for="phone">Votre téléphone</label>
+                <input type="tel" id="phone" name="phone" aria-label="format à 10 chiffre" placeholder="téléphone" aria-required="true">
+                <small>Format: (+33)01-75-22-15-45</small>
+                <button type="submit" value="Envoyer" aria-label="cliquez pour envoyer">envoyer</button>
+            </form>
+        </fieldset>
+</div>
